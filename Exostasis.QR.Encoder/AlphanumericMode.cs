52d5296b138493f14d30600afab4652675a079d3
exo <exo@EXO-PC>
2016-08-17 22:05:41 -0500

    Add project files.

---
+++ Exostasis.QR.Encoder/AlphanumericMode.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exostasis.QR.Encoder
+{
+    public class AlphanumericMode : EncoderBase
+    {
+        public AlphanumericMode(string UnencodedString)
+        {
+            _modeIndicator = new BitArray(BitConverter.GetBytes(0x02));
+            _modeIndicator.Length = 4;           
+            _dataPerBitString = 2;
+            _unencodedString = UnencodedString.ToUpper();
+            _bitsPerBitString = 11;
+            _maximumPossibleCharacterCount = 4926;
+
+            if (_unencodedString.Length > _maximumPossibleCharacterCount)
+            {
+                throw new Exception("This string is unable to be converted into a qr code due to the length of it");
+            }
+
+            base.DetermineMinimumVersionAndMaximumErrorCorrection();
+
+            _characterCountIndicator = new BitArray(BitConverter.GetBytes(_unencodedString.Length));
+            _characterCountIndicator.Length = _bitsPerCharacterCountIndicator;
+        }
+
+        protected override List<BitArray> Encode()
+        {
+            List<BitArray> ListBitArray = new List<BitArray>();
+
+            int length = _unencodedString.Length;
+            int tempValue = 0;
+
+            Byte[] TempByteArray;
+
+            ListBitArray.Add(new BitArray(_modeIndicator));
+            ListBitArray.Add(new BitArray(_characterCountIndicator));       
+
+            for (int i = 0; i < _unencodedString.Length; i += 2)
+            {
+                if (i + 1 >= _unencodedString.Length)
+                {
+                    ListBitArray.Add(new BitArray(6));
+                    tempValue = GetIntValueOfChar(_unencodedString.ElementAt(i));
+                }
+                else
+                {
+                    ListBitArray.Add(new BitArray(_bitsPerBitString));
+                    tempValue = GetIntValueOfChar(_unencodedString.ElementAt(i)) * 45 + GetIntValueOfChar(_unencodedString.ElementAt(i + 1));
+                }
+
+                TempByteArray = BitConverter.GetBytes(tempValue);
+
+                for (int j = 0; j < ListBitArray.Last().Count; j++)
+                {
+                    ListBitArray.Last().Set(j, ((TempByteArray[j/8] & (1 << (j % 8))) != 0));
+                }
+            }
+
+            return ListBitArray;
+        }
+
+        protected override void DetermineBitsPerCharacterCountIndicator()
+        {
+            switch (_version)
+            {
+                case 0:
+                    _bitsPerCharacterCountIndicator = 9;
+                    break;
+                case 9:
+                    _bitsPerCharacterCountIndicator = 11;
+                    break;
+                case 26:
+                    _bitsPerCharacterCountIndicator = 13;
+                    break;
+            }
+        }
+
+        private int GetIntValueOfChar(char Char)
+        {
+            switch(Char)
+            {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    return (int) Char.GetNumericValue(Char);
+                case 'A':
+                    return 10;
+                case 'B':
+                    return 11;
+                case 'C':
+                    return 12;
+                case 'D':
+                    return 13;
+                case 'E':
+                    return 14;
+                case 'F':
+                    return 15;
+                case 'G':
+                    return 16;
+                case 'H':
+                    return 17;
+                case 'I':
+                    return 18;
+                case 'J':
+                    return 19;
+                case 'K':
+                    return 20;
+                case 'L':
+                    return 21;
+                case 'M':
+                    return 22;
+                case 'N':
+                    return 23;
+                case 'O':
+                    return 24;
+                case 'P':
+                    return 25;
+                case 'Q':
+                    return 26;
+                case 'R':
+                    return 27;
+                case 'S':
+                    return 28;
+                case 'T':
+                    return 29;
+                case 'U':
+                    return 30;
+                case 'V':
+                    return 31;
+                case 'W':
+                    return 32;
+                case 'X':
+                    return 33;
+                case 'Y':
+                    return 34;
+                case 'Z':
+                    return 35;
+                case ' ':
+                    return 36;
+                case '$':
+                    return 37;
+                case '%':
+                    return 38;
+                case '*':
+                    return 39;
+                case '+':
+                    return 40;
+                case '-':
+                    return 41;
+                case '.':
+                    return 42;
+                case '/':
+                    return 43;
+                case ':':
+                    return 44;
+            }
+
+            throw new Exception("Invalid char");
+        }
+    }
+}
---
+++ Exostasis.QR.Generator/QRCode.cs
@@ -0,0 +1,61 @@
+﻿using Exostasis.QR.Encoder;
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Exostasis.QR.Generator
+{
+    public class QRCode
+    {
+        private string _UnencodedString { get; set; }
+        private Byte[] _EncodedString { get; set; }
+
+        private EncoderBase _QREncoder { get; set; }
+
+        private const string NumericModeRegex = "^[0-9]+$";
+        private const string AlphanumericModeRegex = "^[0-9a-z$%*+-./:]+$";
+
+        public QRCode(string UnencodedString)
+        {
+            _UnencodedString = UnencodedString;
+        }
+
+        public void Generate ()
+        {
+            _QREncoder = DataAnalyse();
+            _EncodedString = _QREncoder.DataEncode();
+        }
+
+        private EncoderBase DataAnalyse ()
+        {
+            EncoderBase TheEncoder;
+        
+            if (Regex.IsMatch(_UnencodedString, NumericModeRegex))
+            {
+                TheEncoder = new NumericMode();
+            }
+            else if (Regex.IsMatch(_UnencodedString, AlphanumericModeRegex))
+            {
+                TheEncoder = new AlphanumericMode(_UnencodedString);
+            }
+            else if (IsISO8859(_UnencodedString))
+            {
+                TheEncoder = new ByteMode();
+            }
+            else
+            {
+                throw new Exception("Unsuporrted string for QR encoding");
+            }
+
+            return TheEncoder;
+        }
+
+        private bool IsISO8859(string InputString)
+        {
+            byte[] ISOBytes = Encoding.GetEncoding("ISO-8859-1").GetBytes(InputString);
+            string ISOString = Encoding.GetEncoding("ISO-8859-1").GetString(ISOBytes);
+
+            return InputString.Equals(ISOString);
+        }
+    }
+}
