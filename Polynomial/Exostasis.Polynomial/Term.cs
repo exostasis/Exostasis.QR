c00f688dffbfc3260ce1996a0504579ab63d12ee
exo <exo@EXO-PC>
2016-08-18 19:44:36 -0500

    Modified solution to better match the what it really is

---
+++ Polynomial/Exostasis.Polynomial/Term.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exostasis.Equations
+{
+    public class Term
+    {
+        public int _constant { get; private set; }
+        public Variable _variable { get; private set; }
+
+        public Term (int constant)
+        {
+            _constant = constant;
+        }
+    }
+}
--- Polynomial/Exostasis.Polynomial/Variable.cs
+++ Polynomial/Exostasis.Polynomial/Variable.cs
@@ -6,17 +6,21 @@ using System.Threading.Tasks;
 
 namespace Exostasis.Equations
 {
-    public class Term
+    public class Variable
     {
-        public int _constant { get; private set; }
         public int _exponent { get; private set; }
         public string _variable { get; private set; }
 
-        public Term (int constant)
+        public Variable (string variable)
         {
-            _constant = constant;
             _exponent = 1;
-            _variable = null;
+            _variable = variable;
+        }
+
+        public Variable (string variable, int exponent)
+        {
+            _exponent = exponent;
+            _variable = variable;
         }
     }
 }
